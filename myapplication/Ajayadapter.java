c1fdcba0ae6ff82950bda47dbe8453ca28d1c85c
ajay6217 <70676348+ajay6217@users.noreply.github.com>
2022-02-22 21:10:29 +0530

    Update Ajayadapter.java

--- myapplication/Ajayadapter.java
+++ myapplication/Ajayadapter.java
@@ -6,15 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class Ajayadapter extends ArrayAdapter<String> {
     private String[] arr;
+    private Context context;
 
     public Ajayadapter(@NonNull Context context, int resource, @NonNull String[] arr) {
         super(context, resource, arr);
+        this.context=context;
         this.arr=arr;
 
     }
@@ -31,6 +34,14 @@ public class Ajayadapter extends ArrayAdapter<String> {
         convertView= LayoutInflater.from(getContext()).inflate(R.layout.adplayout,parent,false);
         TextView t=convertView.findViewById(R.id.textView);
         t.setText(getItem(position));
+        // clicklistener 
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+                
+            }
+        });
         return convertView;
     }
 }
