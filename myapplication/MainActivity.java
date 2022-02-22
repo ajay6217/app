a33cd1b779075b81c2b4d242129a27b83e285d8d
ajay6217 <70676348+ajay6217@users.noreply.github.com>
2022-02-22 21:11:30 +0530

    Update MainActivity.java

--- myapplication/MainActivity.java
+++ myapplication/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
@@ -15,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.listView);
-        //  ArrayAdapter ad= new ArrayAdapter(this, android.R.layout.simple_list_item_1,arr);
+        //ArrayAdapter ad= new ArrayAdapter(this, android.R.layout.simple_list_item_1,arr);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            // for built in adapter
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "you", Toast.LENGTH_SHORT).show();
+            }
+        });
         Ajayadapter ad=new Ajayadapter(this,R.layout.adplayout,arr);
-
+        
         listView.setAdapter(ad);
     }
 }
